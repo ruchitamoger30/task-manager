5210b7e420f9fb42600344b0b99615f8333507aa
Ruchita Moger <ruchitamoger@gmail.com>
2026-03-14 13:05:58 +0530

    Added Task API endpoints

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,25 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Temporary task storage
+tasks = []
+
 @app.get("/")
 def home():
     return {"message": "Task Manager API is running"}
+
+@app.get("/tasks")
+def get_tasks():
+    return {"tasks": tasks}
+
+@app.post("/tasks")
+def add_task(task: str):
+    tasks.append(task)
+    return {"message": "Task added", "tasks": tasks}
+
+@app.delete("/tasks")
+def delete_task(task: str):
+    if task in tasks:
+        tasks.remove(task)
+        return {"message": "Task removed", "tasks": tasks}
+    return {"message": "Task not found"}
